c1dab70381d24dcab50f71b9aee413c268b11dad
Elton Law <eltonlaw296@gmail.com>
2022-11-15 23:27:44 +0000

    fix: bad test data

--- test/gorp/core_test.clj
+++ test/gorp/core_test.clj
@@ -16,16 +16,16 @@
 (deftest write-str-test
   (let [data {:a 1 :b "foo" :date (java.util.Date. 122 10 1)}]
     (are [expected opts] (= expected (gorp/write-str data opts))
-         "{\"a\":1,\"b\":\"foo\",\"date\":\"2022-11-01T04:00:00Z\"}"
+         "{\"a\":1,\"b\":\"foo\",\"date\":\"2022-11-01T00:00:00Z\"}"
          {:fmt :json}
 
-         "{\n  \"a\" : 1,\n  \"b\" : \"foo\",\n  \"date\" : \"2022-11-01T04:00:00Z\"\n}"
+         "{\n  \"a\" : 1,\n  \"b\" : \"foo\",\n  \"date\" : \"2022-11-01T00:00:00Z\"\n}"
          {:fmt :json :pretty? true}
 
-         "{:a 1, :b \"foo\", :date #inst \"2022-11-01T04:00:00.000-00:00\"}"
+         "{:a 1, :b \"foo\", :date #inst \"2022-11-01T00:00:00.000-00:00\"}"
          {:fmt :edn}
 
-         "{:a 1, :b \"foo\", :date #inst \"2022-11-01T04:00:00.000-00:00\"}\n"
+         "{:a 1, :b \"foo\", :date #inst \"2022-11-01T00:00:00.000-00:00\"}\n"
          {:fmt :edn :pretty? true}))
   (let [xml-data {:tag :SomeElement
                   :attrs {"xmlns" "somens"}
